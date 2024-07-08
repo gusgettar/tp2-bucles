a5f9fcb574789d98da05054ffab682071ec71e89
Gustavo Gettar <gus.gettar@gmail.com>
2024-07-08 11:26:23 -0300

    agregamos el valor de las celdas en la tabla

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -2,12 +2,17 @@
 const filas = parseInt(prompt("Ingrese numero de filas"))
 const columnas = parseInt(prompt("Ingrese un numero de columnas"))
 
+let celda=filas*columnas
 document.write(`<table> <tbody>`)
-document.write(`<tr>`)
-for(let indiceColumnas = 1; indiceColumnas <= columnas; indiceColumnas++){
+for(let indiceFilas = 1; indiceFilas<= filas; indiceFilas++){
 
-    document.write(`<td>1</td>`)
+    document.write(`<tr>`)
+    for(let indiceColumnas = 1; indiceColumnas <= columnas; indiceColumnas++){
+        
+        document.write(`<td>${celda}</td>`)
+        celda--
+    }
 }
-
+    
 document.write(`</tr>`)
 document.write(`</tbody> </table>`)
